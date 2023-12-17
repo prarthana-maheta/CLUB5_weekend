38cee8cba3e7f8fe813a52f9d0d2f7c09c396e35
prarthana.m <prarthana.m@simformsolutions.com>
2023-12-17 10:51:07 +0530

    latest code

--- dec_10.py
+++ dec_10.py
@@ -12,33 +12,64 @@
 # # """
 # #
 # # # # list: Ordered & Mutable collection of members
-# # # list1=[1,2,3,4,5,6,7,8,91,0]
-# # numbers = [33,12, 0, -125, 44, 33, 4791234, -5592, 33]
-# # # index     0  1    2     3   4
-# # #          -9 -8    -7
+# list1=[1,2,3,4,5,6,7,8,91,0]
+numbers = [33,12, 0, -125, 44, 33, 4791234, -5592, 33.67]
+# index     0  1    2     3   4
+#           -9 -8    -7
+
+# print(numbers[-1])
+# print(numbers[0])
+
 # # # print(len(numbers))
 # # # print(type(numbers))
-# # # s='123456789'
-# # # s=123456789
-# # # print(list(s))
+# s='123456789'
+# s=123456789
+# print(str(s))
+
+# s1='123'
+# print(int(s1))
+# print(list(s))
+
+# slicing:
+#
+# first: start
+# second: end
+# third: step /direction/skip
+
+# numbers = [33,12, 0, -125, 44, 33, 4791234, -5592, 33.67]
+# print(numbers[-2:4:-1])
+
+
 # # print(numbers[2:8])
 # #
 # # print altrenate elements from list
 # #
-# # perform addition between first and last element
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# # print(len(numbers)-1)
+
+# list1 =[33,45,56.89,'23','14','89','56',100,'100']
 #
-# numbers = [33,12, 0, -125, 44, 33, 4791234, -5592, "33"]
+# perform addition between first and last element
+# substration between 5 and 8
+# print reverse of list
+# print from 4 to 8 in reverse
+# print alternate numbers in reverse
+
+
+# numbers = [33,12, 0, -125, 44, 33, 4791234, "ab", '33']
 #
+numbers=['a','b','c','A']
+# print(type(numbers))
+# print(len(numbers))
+# print(sum(numbers))
 # print(min(numbers))
 # print(max(numbers))
-#
-#
+print(numbers)
+print(sorted(numbers,reverse=True))
+print(numbers)
+
+# sum of the list
+# find last element using len()
+# find minimum and max of the list
+# print list in asec
+# print list in desc4
+
+# print(f"sum of list 1 is {sum(numbers)},:gxhshcsvcgvhscbh{numbers}")
